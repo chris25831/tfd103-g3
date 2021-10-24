6180ad6d118f8c5076c4393c56f08d2b981a9fc4
hcc3860 <hcc3860@gmail.com>
2021-10-25 01:11:18 +0800

    建立後台樣板與header

--- src/component/common/manage-header.js
+++ src/component/common/manage-header.js
@@ -1,30 +1,38 @@
 
-Vue.component("my-header", {
+Vue.component("manage-header", {
 template: `
-    <ul id="header_wrapper">
-        <li>
-            <a class="logo" href="" ><img src="./src/images/icons/logo.svg"/></a>
-        </li>
-
-        
-        <li>
-            <a class="info title3" href="">三鐵資訊</a>
-            <a class="secondChoice title3" href="">賽程資訊</a>
-        </li>
-        <li><a href="" class="title3">訓練課程</a></li>
-        <li><a href="" class="title3">討論區</a></li>
-        <li><a href="" class="title3">訂製計畫表</a></li>
-        <li>
-            <a href=""><i class="fas fa-user fa-xl"></i></a>
-            <a href=""><i class="fas fa-shopping-basket fa-xl"></i></a>
-        </li>
-
-    </ul>  
+    <div class="" id="manage-header-wrapper">
+        <div class="logo-wrapper">
+            <a class="logo" href=""><img src="./src/images/icons/logo.svg"/></a>
+        </div>
+        <nav class="manage-nav">
+            <ul class="nav-list">
+                <li class="nav-item">
+                    <a href="" class="manage-link">後台人員管理</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="manage-link">賽程管理</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="manage-link">課程管理</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="manage-link">討論區管理</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="manage-link">會員管理</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="manage-link">登出</a>
+                </li>
+            </ul>
+        </nav>    
+    </div>  
 `,
 });
 
 let header = new Vue({
-    el:'#header',
+    el:'#manage-header',
 
     
 })
