3159baa74bcacbf295ad3798fa388ee2997651a5
hcc3860 <hcc3860@gmail.com>
2021-10-27 19:07:54 +0800

    串接彈跳視窗開啟與收合，建立檢舉按鈕、喜愛按鈕初步功能

---
+++ src/component/common/forum-new-post-modal.js
@@ -0,0 +1,43 @@
+Vue.component("forum-new-post-modal", {
+    template: `
+    <div id="forum-new-post-modal" class="darken-background">
+        <div class="new-article modal">
+            <span class="info">發表文章</span>
+            <div class="cross"> 
+                <i @click="returning" class="fas fa-times fa-2x"></i>
+            </div>
+            <input type="text" class="title" placeholder="請輸入文章標題">
+            <select class="category" name="" id="">
+                <option value="">閒聊</option>
+                <option value="">裝備</option>
+                <option value="">心得</option>
+                <option value="">問題</option>
+            </select>
+            <textarea class="new-article-content" name="" id="" cols="30" rows="10"></textarea>
+            <div class="upload-images">
+                <div class="single-image">
+                    <div class="rectangle"></div>
+                </div>
+                <div class="single-image">
+                    <div class="rectangle"></div>
+                </div>
+                <div class="single-image">
+                    <div class="rectangle"></div>
+                </div>
+            </div>
+            
+            <button class="send" type="submit">送出</button>
+        </div>
+    </div>
+    `,
+    // data() {
+
+    // },
+    methods: {
+        returning() {
+            this.$emit("closing");
+        },
+    },
+});
+
+
