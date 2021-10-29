cc93236d27647dcd14de0009e07426b3a687ef05
hcc3860 <hcc3860@gmail.com>
2021-10-29 20:56:09 +0800

    construct basic responsive function for forum pages

--- src/component/common/manage-header.js
+++ src/component/common/manage-header.js
@@ -1,38 +1,37 @@
-
 Vue.component("manage-header", {
-template: `
-    <div class="" id="manage-header-wrapper">
-        <div class="logo-wrapper">
-            <a class="logo" href=""><img src="./src/images/icons/logo.svg"/></a>
-        </div>
-        <nav class="manage-nav">
-            <ul class="nav-list">
-                <li class="nav-item">
-                    <a href="../../../manage-employees.html" class="manage-link">後台人員管理</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="manage-link">賽程管理</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="manage-link">課程管理</a>
-                </li>
-                <li class="nav-item">
-                    <a href="../../../manage-forum-home.html" class="manage-link">討論區管理</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="manage-link">會員管理</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="manage-link">登出</a>
-                </li>
-            </ul>
-        </nav>    
-    </div>  
-`,
+    template: `
+        <div class="" id="manage-header-wrapper">
+            <div class="logo-wrapper">
+                <a class="logo" href="">
+                    <img src="./src/images/icons/logo.svg"/>
+                </a>
+            </div>
+            <nav class="manage-nav">
+                <ul class="nav-list">
+                    <li class="nav-item">
+                        <a href="../../../manage-employees.html" class="manage-link">後台人員管理</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="manage-link">賽程管理</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="manage-link">課程管理</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="../../../manage-forum-home.html" class="manage-link">討論區管理</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="manage-link">會員管理</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="manage-link">登出</a>
+                    </li>
+                </ul>
+            </nav>    
+        </div>  
+    `,
 });
 
 let header = new Vue({
-    el:'#manage-header',
-
-    
+    el:'#manage-header',   
 })
