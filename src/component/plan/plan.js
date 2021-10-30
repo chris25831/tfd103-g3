eddd2f1253c853218f5ba96a2fdd92a9b7787d4e
maggiewang0120 <melody0120h@gmail.com>
2021-10-30 13:14:03 +0800

    header and plan question 80%

--- src/component/plan/plan.js
+++ src/component/plan/plan.js
@@ -3,9 +3,6 @@
 
 
 // ***lightbox*** //
-
-
-
 const question = new Vue({
     el: '#plan_question',
     data(){
@@ -90,14 +87,22 @@ const question = new Vue({
     },
 
     methods: {
+        last(){
+            if(this.current_tab <= this.max  && this.current_tab > 0){
+                this.current_tab --     
+            }
+        },
         currenttab(){
-                if(this.current_tab < this.max){
-                    return  this.current_tab ++
+                if(this.current_tab < this.max && this.current_tab > 0){
+                    this.current_tab ++
+                }
+                if(this.current_tab == 4){
+                    return this.tab ="完成"  //有傳回值就要return
                 }else{
-                    return  this.tab = "送出"
+                    return this.tab ="下一步"
                 }
             }
-      },       
+      }, 
 });
 
 
