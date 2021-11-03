273a4811f35f063f3a7d8e1d0f6956c9c3c92e4e
hcc3860 <hcc3860@gmail.com>
2021-11-03 20:31:43 +0800

    add bookmarkClicked effect

--- src/component/common/bookmark-forum.js
+++ src/component/common/bookmark-forum.js
@@ -1,7 +1,18 @@
 Vue.component("bookmark-forum",{
     props: ["category"],
     template: `
-        <span class="bookmark-item">{{category}}</span>
+        <span class="bookmark-item"> {{category}}</span>
     `,
-     
+    methods: {
+        
+        bookmarkClicked(e){
+            document.querySelectorAll("span.bookmark-item").forEach((text, index) => {
+                if(e.target === text) {
+                    text.style.color = "#ffa10a";
+                } else {
+                    text.style.color = "";
+                }
+            })
+        },
+    },
 });
