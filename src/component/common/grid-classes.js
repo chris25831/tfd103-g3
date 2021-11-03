60184e310efbbd4ea6c6f9f907c311d89e91ce8a
chris25831 <chris25831@gmail.com>
2021-11-04 02:51:25 +0800

    classes-home連結content  購物車初架構

--- src/component/common/grid-classes.js
+++ src/component/common/grid-classes.js
@@ -1,14 +1,24 @@
 // 訓練課程grid
 Vue.component("triclasses",{
-    props: ["classUrl","imgSrc","classTitle","classSubtitle"],
+    props: ["classUrl","imgSrc","classTitle","classSubtitle","classId"],
     template: `
     <div class="classesgrid_simple">
-        <a :href="classUrl"></a> 
+        <a @click="goToContent()"></a> 
         <p> 
+            
             <span class="title3">{{classTitle}}</span><br>
             <span class="article">{{classSubtitle}}</span>
         </p>
         <img :src="imgSrc" alt="無法讀取照片" />       
     </div>
     `,
+    methods:{
+        goToContent(){
+            let url = this.classUrl;
+            let id = this.classId;
+           
+           window.location.href=`${url}?classid=${id}`;
+        }
+
+    },
 });
