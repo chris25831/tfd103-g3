854836c180cc92e1f17820c452946505ef969ab4
hcc3860 <hcc3860@gmail.com>
2021-11-03 20:36:08 +0800

    add bookmark color

--- src/component/common/bookmark-forum.js
+++ src/component/common/bookmark-forum.js
@@ -1,12 +1,12 @@
 Vue.component("bookmark-forum",{
     props: ["category"],
     template: `
-        <span class="bookmark-item"> {{category}}</span>
+        <span class="bookmark-item" @click="bookmarkClicked"> {{category}}</span>
     `,
     methods: {
-        
         bookmarkClicked(e){
             document.querySelectorAll("span.bookmark-item").forEach((text, index) => {
+                console.log("object");
                 if(e.target === text) {
                     text.style.color = "#ffa10a";
                 } else {
