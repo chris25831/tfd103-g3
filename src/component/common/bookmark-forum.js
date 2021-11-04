8a03ec1913aea73a11d0ad4c0147230e6562538d
hcc3860 <hcc3860@gmail.com>
2021-11-04 21:53:49 +0800

    update

--- src/component/common/bookmark-forum.js
+++ src/component/common/bookmark-forum.js
@@ -8,11 +8,24 @@ Vue.component("bookmark-forum",{
             document.querySelectorAll("span.bookmark-item").forEach((text, index) => {
                 if(e.target === text) {
                     text.style.color = "#ffa10a";
-                    
                 } else {
                     text.style.color = "";
                 }
             })
+
+            if(e.target.innerText === "會員基本資料") {
+                window.location.href='./manage-member-personal-info.html';
+            } else if (e.target.innerText === "訂單管理") {
+                window.location.href='./manage-order-list.html';
+            } else if(e.target.innerText === "黑名單管理") {
+                window.location.href='./manage-black-list.html';
+            } else if(e.target.innerText === "檢舉") {
+                window.location.href='./manage-forum-report.html';
+            } else if(e.target.innerText === "檢舉") {
+                window.location.href='./manage-forum-home.html';
+            } else if(e.target.innerText === "公告") {
+                window.location.href='./manage-forum-home.html';
+            }
         },
     },
     mounted() { 
