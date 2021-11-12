0b54273056f72e6c21a1a47855290f720bac6c85
hcc3860 <hcc3860@gmail.com>
2021-11-12 17:31:03 +0800

    製作圖片拖曳功能

--- src/component/common/forum-new-post-modal.js
+++ src/component/common/forum-new-post-modal.js
@@ -6,28 +6,31 @@ Vue.component("forum-new-post-modal", {
             <div class="cross"> 
                 <i @click="returning" class="fas fa-times fa-2x"></i>
             </div>
-            <input type="text" class="title" placeholder="請輸入文章標題">
+            <input type="text" class="title forum-title" placeholder="請輸入文章標題">
             <select class="category" name="" id="">
                 <option value="" disabled selected>請選擇分類</option>
-                <option value="">閒聊</option>
-                <option value="">裝備</option>
-                <option value="">心得</option>
-                <option value="">問題</option>
+                <option value="閒聊">閒聊</option>
+                <option value="裝備">裝備</option>
+                <option value="心得">心得</option>
+                <option value="問題">問題</option>
             </select>
             <textarea class="new-article-content" name="" id="" cols="30" rows="10"></textarea>
             <div class="upload-images">
-                <div class="single-image">
-                    <div class="rectangle"></div>
+                <div class="single-image image-one-wrapper" @dragover.prevent @drop.prevent="dropFirstImage">
+                    <div class="rectangle rectangle-one"></div>
+                    <img class="the-image-one the-image" src="">
                 </div>
-                <div class="single-image">
-                    <div class="rectangle"></div>
+                <div class="single-image image-two-wrapper" @dragover.prevent @drop.prevent="dropSecondImage">
+                    <div class="rectangle rectangle-two"></div>
+                    <img class="the-image-two the-image" src="">
                 </div>
-                <div class="single-image">
-                    <div class="rectangle"></div>
+                <div class="single-image image-three-wrapper" @dragover.prevent @drop.prevent="dropThirdImage">
+                    <div class="rectangle rectangle-three"></div>
+                    <img class="the-image-three the-image" src="">
                 </div>
             </div>
             
-            <button class="send" type="submit">送出</button>
+            <button class="send-forum-content" type="submit">送出</button>
         </div>
     </div>
     `,
@@ -38,6 +41,69 @@ Vue.component("forum-new-post-modal", {
         returning() {
             this.$emit("closing");
         },
+        dropFirstImage(e) {
+            let file = e.dataTransfer.files[0];
+            let readFile = new FileReader();
+
+            let rectangle = document.querySelector(".rectangle");
+            let imageOne = document.querySelector(".image-one-wrapper");
+            let theImage = document.querySelector("img.the-image-one"); 
+
+            readFile.readAsDataURL(file);
+            
+            readFile.addEventListener("load", function(){    
+                
+                    theImage.src = readFile.result;
+                    theImage.style.width = "100%";
+                    theImage.style.height = "100%";
+                    theImage.style.objectFit = "contain"; 
+                    rectangle.style.display = "none";
+                    imageOne.appendChild(theImage);
+                
+            })         
+        },
+        dropSecondImage(e) {
+            let file = e.dataTransfer.files[0];
+            let readFile = new FileReader();
+
+            let rectangleTwo = document.querySelector(".rectangle-two");
+            let imageOne = document.querySelector(".image-two-wrapper");
+            let theImage = document.querySelector("img.the-image-two"); 
+
+            readFile.readAsDataURL(file);
+            
+            readFile.addEventListener("load", function(){    
+                
+                    theImage.src = readFile.result;
+                    theImage.style.width = "100%";
+                    theImage.style.height = "100%";
+                    theImage.style.objectFit = "contain"; 
+                    rectangleTwo.style.display = "none";
+                    imageOne.appendChild(theImage);
+                
+            })  
+        },
+        dropThirdImage(e) {
+            let file = e.dataTransfer.files[0];
+            let readFile = new FileReader();
+
+            let rectangleThree = document.querySelector(".rectangle-three");
+            let imageOne = document.querySelector(".image-three-wrapper");
+            let theImage = document.querySelector("img.the-image-three"); 
+
+            readFile.readAsDataURL(file);
+            
+            readFile.addEventListener("load", function(){    
+                
+                    theImage.src = readFile.result;
+                    theImage.style.width = "100%";
+                    theImage.style.height = "100%";
+                    theImage.style.objectFit = "contain"; 
+                    rectangleThree.style.display = "none";
+                    imageOne.appendChild(theImage);
+                
+            })
+        }
     },
 });
 
