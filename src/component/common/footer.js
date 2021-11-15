145f5c1e2e8393d781c7d81d33c1d3166d5280ea
maggiewang0120 <melody0120h@gmail.com>
2021-11-16 00:36:43 +0800

    後台課程內頁更新

--- src/component/common/footer.js
+++ src/component/common/footer.js
@@ -1,51 +1,4 @@
-// Vue.component("my-footer", {
 
-//     template: `<div id="footer_wrapper">
-      
-//     <div id="footerlogo">
-//         <a class="logo" href="" ><img src="./src/images/icons/logo.svg"/></a>
-//     </div>
-
-//     <div class="sitemap">
-        
-//         <ul>
-//             <p>三鐵資訊</p>
-//             <li>近期賽程</li>
-//             <li>三鐵資訊</li>
-//         </ul>
-//         <ul>
-//             <p>訓練課程</p>
-//             <li>團練課程</li>
-//             <li>個人教練</li>
-//             <li>營養菜單</li>
-//         </ul>
-//         <ul>
-//             <p>討論區</p>
-//             <li>心得</li>
-//             <li>問題</li>
-//             <li>裝備</li>
-//             <li>閒聊</li>
-//         </ul>
-//     </div>
-
-//     <div class="contactus">
-//         <p>聯絡我們<br>
-//         (02)2470-8053</p>
-//         <div>
-//             <a href=""><i class="fa-brands fa-facebook-square fa-2xl" ></i></a>
-//             <a href=""><i class="fa-solid fa-envelope fa-2xl"></i></a>
-//             <a href=""><i class="fa-brands fa-line fa-2xl"></i></a>
-//         </div>
-//     </div>
-
-//     <div>
-//         <p class="small">
-//             本網站為緯育TibaMe前端設計工程師班第70期第__組學員專題成果作品，本平台僅供學習、展示之用。參考資源：＿＿＿＿＿＿＿＿（請填入你們參考的網站）。若有侵權疑慮，您可以私訊 [ 緯育TibaMe ]，後續會由專人協助處理
-//         </p>
-//     </div>
-// </div>
-//     `,
-// });
 let footer = new Vue({
     el:'#footer',
   
@@ -63,45 +16,59 @@ let footer = new Vue({
         template:
             `<div id="footer_wrapper">
           
-            <div id="footerlogo">
-            
-                <a class="logo" href="" ><img src="./src/images/icons/logo.svg"/></a>
-            </div>
-        
-            <div class="sitemap">
+                <div id="footerlogo">
                 
-                <ul>
-                    
-                    <li v-for="info in information">{{info}}</li>
-                    
-                </ul>
-                <ul>
-                    
-                    <li v-for="course in classes">{{course}}</li>
-                    
-                </ul>
-                <ul>
-                    
-                    <li v-for="article in club">{{article}}</li>
-                    
-                </ul>
-            </div>
-        
-            <div class="contactus">
-                <p>聯絡我們<br>
-                (02)2470-8053</p>
-                <div>
-                    <a href=""><i class="fa-brands fa-facebook-square fa-2xl" ></i></a>
-                    <a href=""><i class="fa-solid fa-envelope fa-2xl"></i></a>
-                    <a href=""><i class="fa-brands fa-line fa-2xl"></i></a>
+                    <a class="logo" href="./index.html" ><img src="./src/images/icons/logo.svg"/></a>
                 </div>
-            </div>
         
-            <div>
-                <p class="small">
-                    本網站為緯育TibaMe前端設計工程師班第70期第__組學員專題成果作品，本平台僅供學習、展示之用。參考資源：＿＿＿＿＿＿＿＿（請填入你們參考的網站）。若有侵權疑慮，您可以私訊 [ 緯育TibaMe ]，後續會由專人協助處理
-                </p>
-            </div>
+                <div class="sitemap">
+                    
+                    <ul>
+                        <li>
+                            <a href="./race-home.html">近期賽事</a>
+                        </li>
+                        
+                        
+                        <li>
+                            <a href="./information.html">三鐵資訊</a>
+                        </li>
+                       
+                    </ul>
+                    <ul>
+                       
+                            <li>
+                                <a>團練課程</a>
+                            </li>
+                            <li>
+                                <a>個人教練</a>
+                            </li>
+                            <li>
+                                <a>營養菜單</a>
+                            </li>
+                        
+                    </ul>
+                    <ul>
+                        
+                        <li v-for="article in club">{{article}}</li>
+                        
+                    </ul>
+                </div>
+        
+                <div class="contactus">
+                    <p>聯絡我們<br>
+                    (02)2470-8053</p>
+                    <div>
+                        <a href=""><i class="fa-brands fa-facebook-square fa-2xl" ></i></a>
+                        <a href=""><i class="fa-solid fa-envelope fa-2xl"></i></a>
+                        <a href=""><i class="fa-brands fa-line fa-2xl"></i></a>
+                    </div>
+                </div>
+        
+                <div class="footer_tibame">
+                    <p class="small">
+                        本網站為緯育TibaMe前端設計工程師班第70期第__組學員專題成果作品，本平台僅供學習、展示之用。參考資源：＿＿＿＿＿＿＿＿（請填入你們參考的網站）。若有侵權疑慮，您可以私訊 [ 緯育TibaMe ]，後續會由專人協助處理
+                    </p>
+                </div>
         </div>`
     }
   }
