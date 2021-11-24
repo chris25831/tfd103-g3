22202f6efacfb62c62496e45037235612c40513f
hcc3860 <hcc3860@gmail.com>
2021-11-24 19:37:51 +0800

    討論區發文新增對登入阻擋

--- src/component/common/manage-header.js
+++ src/component/common/manage-header.js
@@ -19,19 +19,15 @@ Vue.component("manage-header", {
                         <a href="./manage-member-personal-info.html" class="manage-link">會員管理</a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="manage-link" v-if="logouthref" @click="logout">登出</a>
+                        <a href="#" class="manage-link" v-if="logoutHref" @click="logout">登出</a>
                     </li>
                 </ul>
             </nav>    
         </div>  
     `,
-});
-
-let header = new Vue({
-    el:'#manage-header',
     data(){
         return {
-            logouthref: false,
+            logoutHref: false,
         }
     },
     methods: {
@@ -40,43 +36,50 @@ let header = new Vue({
                 let MemberID = this.memberID;
             }
             axios({            
-              method: "POST",
-              url: "./php/logout.php",
-              data:{},  
-              })
-              .then((response) => {
-                  if(response.data){
-                      alert("已登出"); 
-                      this.logouthref = false;
-                      this.memberID = "";
-                      location.href = 'index.html';
-                  }else{
-                     alert('登出失敗QQ請重新執行'); 
-                  }              
-              })
-              .catch((error)=>{
-                  console.log("錯誤")
-              });
+                method: "POST",
+                url: "./php/logout.php",
+                data:{},  
+                })
+                .then((response) => {
+                    if(response.data){
+                        alert("已登出"); 
+                        this.logoutHref = false;
+                        this.memberID = "";
+                        location.href = 'index.html';
+                    }else{
+                        alert('登出失敗QQ請重新執行'); 
+                    }              
+                })
+                .catch((error)=>{
+                    console.log(error);
+                    console.log("錯誤")
+                });
         }
     },
     mounted(){
         axios({            
             method: "POST",
             url: "./php/loginCheck.php",
-          }).then((response) => {
-          // console.log(response.data)
+        }).then((response) => {
+        // console.log(response.data)
             if(response.data === ""){
                 console.log('未登入')
-                  // alert('請先登入，將前往登入頁'); 
-                  // location.href = 'Login.html';
-              }else{
-                  console.log("有登入")
-                  console.log(response.data)
-                  this.logouthref = true;
-                  this.memberID = response.data
-              } 
-          }).catch(function(error){
+                    // alert('請先登入，將前往登入頁'); 
+                    // location.href = 'Login.html';
+            }else{
+                console.log("有登入")
+                console.log(response.data)
+                this.logoutHref = true;
+                console.log(this.logoutHref);
+                this.memberID = response.data
+            } 
+        }).catch(function(error){
             console.log("錯誤");
-          })    
+        })    
     },   
+});
+
+let header = new Vue({
+    el:'#manage-header',
+    
 })
